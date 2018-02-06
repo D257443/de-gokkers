481f898373f38acb0c8345238bd1e8ed075c1e56
Unknown <D257443@edu.rocwb.nl>
2018-02-06 12:59:54 +0100

    Fix
    Fixed

--- TheReal-de-gokkers-project/TheReal-de-gokkers-project/Form1.Designer.cs
+++ TheReal-de-gokkers-project/TheReal-de-gokkers-project/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -42,56 +41,32 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-=======
-<<<<<<< Updated upstream
-            this.button1 = new System.Windows.Forms.Button();
-=======
-<<<<<<< HEAD
->>>>>>> Stashed changes
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(363, 58);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 126);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
->>>>>>> master
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
+
             this.ClientSize = new System.Drawing.Size(600, 527);
             this.Controls.Add(this.pictureBox1);
-=======
+
             this.ClientSize = new System.Drawing.Size(1139, 261);
-            this.Controls.Add(this.button1);
->>>>>>> master
+
             this.Name = "Form1";
-=======
+
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
->>>>>>> parent of dd578db... olo
-            this.Text = "Form1";
-<<<<<<< HEAD
+
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
-=======
->>>>>>> master
+
         }
 
         #endregion
 
-<<<<<<< HEAD
+
         private System.Windows.Forms.PictureBox pictureBox1;
-=======
-        private System.Windows.Forms.Button button1;
->>>>>>> master
     }
 }
 
