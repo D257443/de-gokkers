d1071ef52212e2e9c0980ea1c779976bf91af0c4
Red <gijs.horsten1@gmail.com>
2018-02-06 15:57:36 +0100

    6-2-2018 Layout 16:00
    De layout is gemaakt, nog niet af volgens het ontwerp

--- DeGokkers/DeGokkers/Form1.Designer.cs
+++ DeGokkers/DeGokkers/Form1.Designer.cs
@@ -28,12 +28,384 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDamien = new System.Windows.Forms.Label();
+            this.lblGijs = new System.Windows.Forms.Label();
+            this.lblDave = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inpAnNum = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inpMoney = new System.Windows.Forms.NumericUpDown();
+            this.btnBet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDave = new System.Windows.Forms.RadioButton();
+            this.btnDamien = new System.Windows.Forms.RadioButton();
+            this.btnGijs = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inpAnNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpMoney)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DeGokkers.Properties.Resources.PictureBox1;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.lblDamien);
+            this.groupBox1.Controls.Add(this.lblGijs);
+            this.groupBox1.Controls.Add(this.lblDave);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.inpAnNum);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.inpMoney);
+            this.groupBox1.Controls.Add(this.btnBet);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnDave);
+            this.groupBox1.Controls.Add(this.btnDamien);
+            this.groupBox1.Controls.Add(this.btnGijs);
+            this.groupBox1.Location = new System.Drawing.Point(12, 206);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(577, 263);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Wed Bureau";
+            // 
+            // lblDamien
+            // 
+            this.lblDamien.AutoSize = true;
+            this.lblDamien.BackColor = System.Drawing.SystemColors.Window;
+            this.lblDamien.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDamien.Location = new System.Drawing.Point(369, 126);
+            this.lblDamien.MinimumSize = new System.Drawing.Size(200, 0);
+            this.lblDamien.Name = "lblDamien";
+            this.lblDamien.Size = new System.Drawing.Size(200, 15);
+            this.lblDamien.TabIndex = 9;
+            this.lblDamien.Text = "Damien heeft";
+            // 
+            // lblGijs
+            // 
+            this.lblGijs.AutoSize = true;
+            this.lblGijs.BackColor = System.Drawing.SystemColors.Window;
+            this.lblGijs.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblGijs.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.lblGijs.Location = new System.Drawing.Point(369, 103);
+            this.lblGijs.MinimumSize = new System.Drawing.Size(200, 0);
+            this.lblGijs.Name = "lblGijs";
+            this.lblGijs.Size = new System.Drawing.Size(200, 15);
+            this.lblGijs.TabIndex = 8;
+            this.lblGijs.Text = "Gijs heeft";
+            // 
+            // lblDave
+            // 
+            this.lblDave.AutoSize = true;
+            this.lblDave.BackColor = System.Drawing.SystemColors.Window;
+            this.lblDave.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDave.Location = new System.Drawing.Point(369, 149);
+            this.lblDave.MinimumSize = new System.Drawing.Size(200, 0);
+            this.lblDave.Name = "lblDave";
+            this.lblDave.Size = new System.Drawing.Size(200, 15);
+            this.lblDave.TabIndex = 8;
+            this.lblDave.Text = "Dave heeft";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Weddenshappen";
+            // 
+            // inpAnNum
+            // 
+            this.inpAnNum.Location = new System.Drawing.Point(289, 103);
+            this.inpAnNum.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.inpAnNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.inpAnNum.Name = "inpAnNum";
+            this.inpAnNum.Size = new System.Drawing.Size(42, 20);
+            this.inpAnNum.TabIndex = 6;
+            this.inpAnNum.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 105);
+            this.label2.MinimumSize = new System.Drawing.Size(0, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "euro\'s op nummer";
+            // 
+            // inpMoney
+            // 
+            this.inpMoney.Location = new System.Drawing.Point(145, 103);
+            this.inpMoney.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.inpMoney.Name = "inpMoney";
+            this.inpMoney.Size = new System.Drawing.Size(42, 20);
+            this.inpMoney.TabIndex = 4;
+            this.inpMoney.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btnBet
+            // 
+            this.btnBet.Location = new System.Drawing.Point(64, 103);
+            this.btnBet.Name = "btnBet";
+            this.btnBet.Size = new System.Drawing.Size(75, 20);
+            this.btnBet.TabIndex = 3;
+            this.btnBet.Text = "Wedt";
+            this.btnBet.UseVisualStyleBackColor = true;
+            this.btnBet.Click += new System.EventHandler(this.btnBet_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(7, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Minimum Inzet: 5 Euro ";
+            // 
+            // btnDave
+            // 
+            this.btnDave.AutoSize = true;
+            this.btnDave.Location = new System.Drawing.Point(6, 152);
+            this.btnDave.Name = "btnDave";
+            this.btnDave.Size = new System.Drawing.Size(51, 17);
+            this.btnDave.TabIndex = 2;
+            this.btnDave.TabStop = true;
+            this.btnDave.Text = "Dave";
+            this.btnDave.UseVisualStyleBackColor = true;
+            // 
+            // btnDamien
+            // 
+            this.btnDamien.AutoSize = true;
+            this.btnDamien.Location = new System.Drawing.Point(6, 129);
+            this.btnDamien.Name = "btnDamien";
+            this.btnDamien.Size = new System.Drawing.Size(61, 17);
+            this.btnDamien.TabIndex = 1;
+            this.btnDamien.TabStop = true;
+            this.btnDamien.Text = "Damien";
+            this.btnDamien.UseVisualStyleBackColor = true;
+            // 
+            // btnGijs
+            // 
+            this.btnGijs.AutoSize = true;
+            this.btnGijs.Location = new System.Drawing.Point(6, 106);
+            this.btnGijs.Name = "btnGijs";
+            this.btnGijs.Size = new System.Drawing.Size(42, 17);
+            this.btnGijs.TabIndex = 0;
+            this.btnGijs.TabStop = true;
+            this.btnGijs.Text = "Gijs";
+            this.btnGijs.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(289, 126);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 128);
+            this.label4.MinimumSize = new System.Drawing.Size(0, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "euro\'s op nummer";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(145, 126);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown2.TabIndex = 11;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Wedt";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(289, 149);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown3.TabIndex = 17;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 151);
+            this.label5.MinimumSize = new System.Drawing.Size(0, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "euro\'s op nummer";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(145, 149);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(42, 20);
+            this.numericUpDown4.TabIndex = 15;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Wedt";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 481);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inpAnNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpMoney)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton btnGijs;
+        private System.Windows.Forms.RadioButton btnDave;
+        private System.Windows.Forms.RadioButton btnDamien;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown inpAnNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown inpMoney;
+        private System.Windows.Forms.Button btnBet;
+        private System.Windows.Forms.Label lblDamien;
+        private System.Windows.Forms.Label lblGijs;
+        private System.Windows.Forms.Label lblDave;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
