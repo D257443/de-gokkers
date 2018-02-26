90ef24233fe3bf7666c8979c2f5bdaae0bb5b38c
Unknown <D257443@edu.rocwb.nl>
2018-02-26 15:47:19 +0100

    Added pictures - Not working yet
    Added the doggo and new background

--- DeGokkers/DeGokkers/Form1.Designer.cs
+++ DeGokkers/DeGokkers/Form1.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
             this.btnAddName = new System.Windows.Forms.Button();
             this.btnName2 = new System.Windows.Forms.RadioButton();
             this.btnName3 = new System.Windows.Forms.RadioButton();
@@ -55,8 +57,15 @@
             this.btnPlayer3 = new System.Windows.Forms.RadioButton();
             this.btnPlayer2 = new System.Windows.Forms.RadioButton();
             this.btnPlayer1 = new System.Windows.Forms.RadioButton();
-            this.btnStart = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tmrDog = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.picDog1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.inpAnNum3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inpMoney3)).BeginInit();
@@ -64,16 +73,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.inpMoney2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inpAnNum1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.inpMoney1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDog1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::DeGokkers.Properties.Resources.PictureBox1;
-            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(600, 200);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // groupBox1
             // 
@@ -109,6 +114,16 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Wed Bureau";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.btnStart.Location = new System.Drawing.Point(369, 176);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(200, 81);
+            this.btnStart.TabIndex = 23;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
             // 
             // btnAddName
             // 
@@ -428,26 +443,76 @@
             this.btnPlayer1.Text = "Speler1";
             this.btnPlayer1.UseVisualStyleBackColor = true;
             // 
-            // btnStart
+            // tmrDog
             // 
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.btnStart.Location = new System.Drawing.Point(369, 176);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(200, 81);
-            this.btnStart.TabIndex = 23;
-            this.btnStart.Text = "START";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.tmrDog.Interval = 50;
+            // 
+            // picDog1
+            // 
+            this.picDog1.BackColor = System.Drawing.Color.White;
+            this.picDog1.Image = global::DeGokkers.Properties.Resources.dog1;
+            this.picDog1.InitialImage = ((System.Drawing.Image)(resources.GetObject("picDog1.InitialImage")));
+            this.picDog1.Location = new System.Drawing.Point(12, 12);
+            this.picDog1.Name = "picDog1";
+            this.picDog1.Size = new System.Drawing.Size(82, 32);
+            this.picDog1.TabIndex = 2;
+            this.picDog1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DeGokkers.Properties.Resources.grid;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = global::DeGokkers.Properties.Resources.dog1;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 155);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(82, 32);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Image = global::DeGokkers.Properties.Resources.dog1;
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 108);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(82, 32);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Image = global::DeGokkers.Properties.Resources.dog1;
+            this.pictureBox4.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.InitialImage")));
+            this.pictureBox4.Location = new System.Drawing.Point(12, 60);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(82, 32);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(601, 481);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.picDog1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.inpAnNum3)).EndInit();
@@ -456,6 +521,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.inpMoney2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.inpAnNum1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.inpMoney1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDog1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -490,6 +560,14 @@
         private System.Windows.Forms.RadioButton btnName1;
         private System.Windows.Forms.TextBox txtInpName;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tmrDog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox picDog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- DeGokkers/DeGokkers/Properties/Resources.Designer.cs
+++ DeGokkers/DeGokkers/Properties/Resources.Designer.cs
@@ -63,6 +63,36 @@ namespace DeGokkers.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap dog {
+            get {
+                object obj = ResourceManager.GetObject("dog", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap dog1 {
+            get {
+                object obj = ResourceManager.GetObject("dog1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap grid {
+            get {
+                object obj = ResourceManager.GetObject("grid", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap PictureBox1 {
             get {
                 object obj = ResourceManager.GetObject("PictureBox1", resourceCulture);
